259562bb0d4347f86f2f0fc27d2c8df15d1e7976
“OsorioJei” <jeison.osorio48375@ucaldas.edu.co>
2025-04-29 22:48:45 -0500

    demoPrueba-get4

--- demo/src/main/java/the/project/demo/ProjectApplication.java
+++ demo/src/main/java/the/project/demo/ProjectApplication.java
@@ -2,21 +2,18 @@ package the.project.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PostMapping; // Usamos PostMapping en lugar de GetMapping
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ProjectApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
+      SpringApplication.run(ProjectApplication.class, args);
     }
-
-    // Usamos @PostMapping para aceptar peticiones POST
-    @PostMapping("/github-webhook/")
-    public String githubWebhook(@RequestBody String payload) {
-        System.out.println("Webhook recibido: " + payload);  // Aquí procesas el payload que GitHub envía
-        return "Webhook recibido con éxito";
+    @GetMapping("/github-webhook/")
+    public String hello(@RequestParam(value = "name", defaultValue = "hola a que hora nos reunimos el sabado") String name) {
+      return String.format("Hello %s!", name);
     }
 }
