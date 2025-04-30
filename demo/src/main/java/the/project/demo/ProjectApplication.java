bc33bf28cbb3fdea5003287a9a5ba8222708cafb
“OsorioJei” <jeison.osorio48375@ucaldas.edu.co>
2025-04-29 22:33:55 -0500

    demoPrueba-get1

--- demo/src/main/java/the/project/demo/ProjectApplication.java
+++ demo/src/main/java/the/project/demo/ProjectApplication.java
@@ -12,7 +12,7 @@ public class ProjectApplication {
     public static void main(String[] args) {
       SpringApplication.run(ProjectApplication.class, args);
     }
-    @GetMapping("/github-webhook/")
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "vamosBien") String name) {
       return String.format("Hello %s!", name);
     }
