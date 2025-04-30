26c0149b12ca60410d94385bde5a82c0971b0701
“OsorioJei” <jeison.osorio48375@ucaldas.edu.co>
2025-04-29 21:35:37 -0500

    demoPrueba

---
+++ demo/src/main/java/the/project/demo/ProjectApplication.java
@@ -0,0 +1,19 @@
+package the.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(ProjectApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "vamosBien") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
